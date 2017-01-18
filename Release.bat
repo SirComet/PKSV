ae598c48a9f78496b9e28dc7ffcaa0a0d9a06abf
Favna <jeroen.claassens@live.nl>
2017-01-18 15:39:29 +0100

    update release batch file

--- Release.bat
+++ Release.bat
@@ -3,6 +3,6 @@ rd Release /s /q
 mkdir Release\3ds\PKSV
 move output\3ds-arm\PKSV.cia Release
 move output\3ds-arm\3ds\PKSV\PKSV.3dsx Release\3ds\PKSV
-zip -r PKSV.zip Release
-move PKSV.zip Release
-rd output /s /q
+cd Release
+zip -r PKSV.zip .
+cd ..\
