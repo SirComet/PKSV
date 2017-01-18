ea439aeddb23ede88268940c238c5324c96f5732
Favna <jeroen.claassens@live.nl>
2017-01-18 18:43:21 +0100

    Have release also create xml file for Homebrew

--- Release.bat
+++ Release.bat
@@ -3,6 +3,7 @@ rd Release /s /q
 mkdir Release\3ds\PKSV
 copy output\3ds-arm\PKSV.cia Release
 copy output\3ds-arm\3ds\PKSV\PKSV.3dsx Release\3ds\PKSV
+@echo ^<targets selectable="true"^>^</targets^> > Release\3ds\PKSV\PKSV.xml
 cd Release
 zip -r PKSV.zip .
 cd ..\
