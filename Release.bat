63fc40c62b2a30e0cab9f2e370db2bd004db4492
Favna <jeroen.claassens@live.nl>
2017-01-18 15:49:56 +0100

    Fix derp

--- Release.bat
+++ Release.bat
@@ -1,8 +1,9 @@
 make
 rd Release /s /q
 mkdir Release\3ds\PKSV
-move output\3ds-arm\PKSV.cia Release
-move output\3ds-arm\3ds\PKSV\PKSV.3dsx Release\3ds\PKSV
+copy output\3ds-arm\PKSV.cia Release
+copy output\3ds-arm\3ds\PKSV\PKSV.3dsx Release\3ds\PKSV
 cd Release
 zip -r PKSV.zip .
 cd ..\
+pause
